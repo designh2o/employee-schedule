489c3d978bef4fc1bfc76102e518f288ec615f7e
Andrey Dubinin <ald91@mail.ru>
2019-02-19 22:52:31 +0700

    refactoring

--- app/Helpers/Schedule.php
+++ app/Helpers/Schedule.php
@@ -35,7 +35,7 @@ class Schedule
 	public function __construct(User $user, $start, $end)
 	{
 		$this->user = $user;
-		if($start > $end){
+		if ($start > $end) {
 			throw new \Exception('start date greater end date');
 		}
 		$this->period = CarbonPeriod::create($start, $end);
@@ -46,7 +46,8 @@ class Schedule
 	 * Добавление дня
 	 * @param ScheduleDay $day
 	 */
-	public function addDay(ScheduleDay $day){
+	public function addDay(ScheduleDay $day)
+	{
 		$this->days->push($day);
 	}
 
@@ -54,7 +55,8 @@ class Schedule
 	 * Получить количество дней
 	 * @return int
 	 */
-	public function getCountDay(){
+	public function getCountDay()
+	{
 		return $this->days->count();
 	}
 
@@ -62,47 +64,34 @@ class Schedule
 	 * Рассчет рабочих дней
 	 * Выходные дни пропускаем, заполняем рабочие дни рабочим расписанием, потом отнимаем корпоративы и отпуски
 	 */
-	public function calculateWorkDays(){
+	public function calculateWorkDays()
+	{
 		/**
 		 * @var  $key
 		 * @var Carbon $date
 		 */
 		foreach ($this->period as $key => $date) {
 			$day = new ScheduleDay($date);
-			if($day->checkHoliday()){	//проверяем, не выходной ли день
+			if ($day->checkHoliday()) {    //проверяем, не выходной ли день
 				continue;
 			}
-			$this->setWorkScheduleDay($day);	//устанавливаем рабочее расписания для дня
+			$this->setWorkScheduleDay($day);    //устанавливаем рабочее расписания для дня
 			//вычитаем корпоративы
-			if($this->user->company) {	//привязана ли компания к пользователю
-				$corporates = $this->user->company->corporates;
-				if ($corporates) {	//есть ли корпоративы
-					foreach ($corporates as $corporate) {
-						//вычитаем корпоратив из расписания дня
-						if (!$day->differenceRanges($corporate->start, $corporate->end)) {
-							//день получился пустой, не добавляем
-							unset($day);
-							continue 2;	//переходим к следующему дню
-						}
-					}
+			if ($this->user->company) {    //привязана ли компания к пользователю
+				if(!$this->differenceRanges($day, $this->user->company->corporates)){
+					unset($day);
+					continue;
 				}
 			}
 
-			if($this->user->vacations) {
+			if ($this->user->vacations) {
 				//вычитаем отпуски
-				$vacations = $this->user->vacations;
-				if ($vacations) {
-					foreach ($vacations as $vacation) {
-						//вычитаем отпуск из расписания дня
-						if (!$day->differenceRanges($vacation->start, $vacation->end)) {
-							//день пустой, не добавляем
-							unset($day);
-							continue 2;	//переходим к следующему дню
-						}
-					}
+				if(!$this->differenceRanges($day, $this->user->vacations)){
+					unset($day);
+					continue;
 				}
 			}
-			$this->addDay($day);	//добавляем день в расписание
+			$this->addDay($day);    //добавляем день в расписание
 		}
 	}
 
@@ -110,47 +99,30 @@ class Schedule
 	 * Рассчет выходных дней
 	 * Добавляем выходные дни, рабочие дни заполняем обычным графиком, потом инвертируем диапозоны времени
 	 */
-	public function calculateFreeDays(){
+	public function calculateFreeDays()
+	{
 		/**
 		 * @var  $key
 		 * @var Carbon $date
 		 */
 		foreach ($this->period as $key => $date) {
-			$needInverse = false;	//флаг, нужно ли инверитровать диапозоны
+			$needInverse = false;    //флаг, нужно ли инверитровать диапозоны
 			$day = new ScheduleDay($date);
-			if($day->checkHoliday()) {	//выходной - добавляем полностью
+			if ($day->checkHoliday()) {    //выходной - добавляем полностью
 				$this->setFullScheduleDay($day);
-			}else{	//не выходной - считаем рабочее время и инвертируем диапозоны
+			} else {    //не выходной - считаем рабочее время и инвертируем диапозоны
 				$this->setWorkScheduleDay($day);
 				//вычитаем корпоративы
-				if($this->user->company) {
-					$corporates = $this->user->company->corporates;
-					if ($corporates) {
-						foreach ($corporates as $corporate) {
-							//вычитаем корпоратив из расписания дня
-							if ($day->differenceRanges($corporate->start, $corporate->end)) {
-								//если день получился не пустой, значит потребуется инвертировать его
-								$needInverse = true;
-							}
-						}
-					}
+				if ($this->user->company) {
+					$needInverse = $this->differenceRanges($day, $this->user->company->corporates, true);
 				}
-
-				if($this->user->vacations) {
+				if ($this->user->vacations) {
 					//вычитаем отпуски
-					$vacations = $this->user->vacations;
-					if ($vacations) {
-						foreach ($vacations as $vacation) {
-							if ($day->differenceRanges($vacation->start, $vacation->end)) {
-								//инвертируем
-								$needInverse = true;
-							}
-						}
-					}
+					$needInverse = $this->differenceRanges($day, $this->user->vacations, true);
 				}
-				if($needInverse){	//день оказался непустой, инвертируем временные диапозоны
+				if ($needInverse) {    //день оказался непустой, инвертируем временные диапозоны
 					$day->inverseRange();
-				}else{	//день оказался пустой, добавляем весь день
+				} else {    //день оказался пустой, добавляем весь день
 					$this->setFullScheduleDay($day);
 				}
 			}
@@ -158,12 +130,38 @@ class Schedule
 		}
 	}
 
-	public function toArray(){
+	/**
+	 * Отнимаем из дня период
+	 * @param ScheduleDay $day
+	 * @param Collection $rangeCollection
+	 * @param bool $needInverse нужно ли далее инвертировать день
+	 * @return bool
+	 */
+	protected function differenceRanges(ScheduleDay $day, Collection $rangeCollection, $needInverse = false)
+	{
+		$notEmptyDay = false;
+		foreach ($rangeCollection as $range) {
+			if ($day->differenceRanges($range->start, $range->end)) {
+				if($needInverse){
+					$notEmptyDay = true;
+				}else{
+					return false;
+				}
+			}
+		}
+		if($needInverse){
+			return $notEmptyDay;
+		}
+		return true;
+	}
+
+	public function toArray()
+	{
 		$result = [
 			'schedule' => [],
 		];
 		/** @var ScheduleDay $day */
-		foreach($this->days as $day){
+		foreach ($this->days as $day) {
 			$result['schedule'][] = $day->toArray();
 		}
 		return $result;
@@ -174,10 +172,11 @@ class Schedule
 	 * @param ScheduleDay $day
 	 * @return ScheduleDay
 	 */
-	protected function setWorkScheduleDay(ScheduleDay $day){
+	protected function setWorkScheduleDay(ScheduleDay $day)
+	{
 		$scheduleDay = $this->user->schedule_day;
-		if(isset($scheduleDay['work'])) {
-			if(!isset($scheduleDay['lunch'])){	//без обеда:(
+		if (isset($scheduleDay['work'])) {
+			if (!isset($scheduleDay['lunch'])) {    //без обеда:(
 				$day->addRange(TimeRange::create(
 					$day->getDay()->setTime(
 						$this->user->schedule_day['work']['start']['hour'],
@@ -186,7 +185,7 @@ class Schedule
 						$this->user->schedule_day['work']['end']['hour'],
 						$this->user->schedule_day['work']['end']['minute'])
 				));
-			}else {
+			} else {
 				$day->addRange(TimeRange::create(
 					$day->getDay()->setTime(
 						$this->user->schedule_day['work']['start']['hour'],
@@ -213,10 +212,11 @@ class Schedule
 	 * @param ScheduleDay $day
 	 * @return ScheduleDay
 	 */
-	protected function setFullScheduleDay(ScheduleDay $day){
+	protected function setFullScheduleDay(ScheduleDay $day)
+	{
 		$day->addRange(TimeRange::create(
 			$day->getDay()->setTime(0, 0),
-			$day->getDay()->setTime(23,59)));
+			$day->getDay()->setTime(23, 59)));
 		return $day;
 	}
 }
